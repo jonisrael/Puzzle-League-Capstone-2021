30887bece17b279b50b7c1cdbaf086d23a245a30
Jonathan Israel <jonisrael45@gmail.com>
2021-09-09 15:31:58 -0500

    new video content added

--- components/views/Tutorials.js
+++ components/views/Tutorials.js
@@ -2,7 +2,10 @@ import html from "html-literal";
 
 export default st => html`
   <section id="tutorials-page">
-    <h1 id="tutorials-header">How to Play</h1>
+    <h1 id="tutorials-header" style="font-size: xx-large;">
+      Tutorials (3 currently)
+    </h1>
+    <h2>How to play</h2>
     <div id="container">
       <iframe
         width="960"
@@ -14,5 +17,31 @@ export default st => html`
         allowfullscreen
       ></iframe>
     </div>
+    <br />
+    <hr color="red" />
+    <br />
+    <h2>Intermediate Chain Example & Explanation</h2>
+    <iframe
+      width="960"
+      height="540"
+      src="https://www.youtube.com/embed/3do_FYRj0ms"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <br />
+    <hr color="red" />
+    <br />
+    <h2>Advanced 9x Chain Example</h2>
+    <iframe
+      width="960"
+      height="540"
+      src="https://www.youtube.com/embed/XqHQl1OB-cw"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
   </section>
 `;
