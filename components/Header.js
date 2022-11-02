981763858da6bb7c7e0972e31dd9125345d97b6c
jonisrael <72095830+jonisrael@users.noreply.github.com>
2022-11-02 14:42:16 -0500

    update date

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import html from "html-literal";
 export default (st) => html`
   <header id="header">
     <h1 id="pla-title">Puzzle League Arcade</h1>
-    <h2 id=version-release>Last Update: 10/28/22</h2>
+    <h2 id=version-release>Last Update: 11/02/22</h2>
     <h1 id="current-page" style="color:white">${st.header}</h1>
 
     <ul id="sound-mute-menu">
