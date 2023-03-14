dd59e46a8be524efe1caa006d1a4efefe2191a7b
jonisrael <72095830+jonisrael@users.noreply.github.com>
2023-03-14 13:54:22 -0500

    try again...

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-export const databaseName = "games";
+const databaseName = "games";
 
 const games = require(`./routers/${databaseName}.js`);
 
